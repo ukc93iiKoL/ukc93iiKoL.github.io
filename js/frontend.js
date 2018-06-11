c3b333236587b30d0381271186c102df338a844c
REo <abbareo@gmail.com>
2018-06-11 18:42:12 +0200

    redeployed

---
+++ js/frontend.js
@@ -0,0 +1,12 @@
+$(document).foundation(), $(document).ready(function() {
+    // $.localScroll.hash(), $.localScroll({
+    //     hash: true,
+    //     offset: {
+    //         top: -63
+    //     }
+    // }),
+    $("a.blank").attr("target", "_blank")
+    // $.get("https://api.facebook.com/method/fql.query?query=select%20like_count%20from%20link_stat%20where%20url=%27http://www.facebook.com/musilieu/%27&format=json", function(a) {
+    //     $("#fb-likes").html(a[0].like_count)
+    // })
+});
